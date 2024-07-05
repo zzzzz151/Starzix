f6acffb58afb11ca3200dccfceb219273ec83172
zzzzz <ricardopinto15@hotmail.com>
2024-07-05 10:55:45 +0100

    New net (MPE 2.5 loss instead of MSE) (#16)
    bench 7393039

--- tests/testsNNUE.cpp
+++ tests/testsNNUE.cpp
@@ -3,37 +3,37 @@
 #include "../src/nnue.hpp"
 
 std::unordered_map<std::string, int> FENS_EVAL = {
-    { START_FEN, 148 },
-    { "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",  -85 }, // e2e4
-    { "rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 2", 113 }, // e2e4 g8f6
+    { START_FEN, 59 },
+    { "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",  -72 }, // e2e4
+    { "rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 2", 103 }, // e2e4 g8f6
 
     // Moves in "r3k2b/6P1/8/1pP5/8/8/4P3/4K2R w Kq b6 0 1"
-    { "r3k2b/6P1/1P6/8/8/8/4P3/4K2R b Kq - 0 1", -411 },
-    { "r3k2b/6P1/8/1pP5/8/4P3/8/4K2R b Kq - 0 1", 71 },
-    { "r3k2b/6P1/8/1pP5/4P3/8/8/4K2R b Kq e3 0 1", 82 },
-    { "r3k2b/6P1/2P5/1p6/8/8/4P3/4K2R b Kq - 0 1", -31 },
-    { "r3k2Q/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -2361 },
-    { "r3k2R/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -1747 },
-    { "r3k2B/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -952 },
-    { "r3k2N/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -531 },
-    { "r3k1Qb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -1531 },
-    { "r3k1Rb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -797 },
-    { "r3k1Bb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -53 },
-    { "r3k1Nb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", 156 },
-    { "r3k2b/6P1/8/1pP5/8/8/4P3/3K3R b q - 1 1", 21 },
-    { "r3k2b/6P1/8/1pP5/8/8/4P3/5K1R b q - 1 1", 75 },
-    { "r3k2b/6P1/8/1pP5/8/8/3KP3/7R b q - 1 1", -50 },
-    { "r3k2b/6P1/8/1pP5/8/8/4PK2/7R b q - 1 1", 19 },
-    { "r3k2b/6P1/8/1pP5/8/8/4P3/5RK1 b q - 1 1", 36 },
-    { "r3k2b/6P1/8/1pP5/8/8/4P3/4KR2 b q - 1 1", -26 },
-    { "r3k2b/6P1/8/1pP5/8/8/4P3/4K1R1 b q - 1 1", 63 },
-    { "r3k2b/6P1/8/1pP5/8/8/4P2R/4K3 b q - 1 1", 66 },
-    { "r3k2b/6P1/8/1pP5/8/7R/4P3/4K3 b q - 1 1", 45 },
-    { "r3k2b/6P1/8/1pP5/7R/8/4P3/4K3 b q - 1 1", 21 },
-    { "r3k2b/6P1/8/1pP4R/8/8/4P3/4K3 b q - 1 1", -43 },
-    { "r3k2b/6P1/7R/1pP5/8/8/4P3/4K3 b q - 1 1", -38 },
-    { "r3k2b/6PR/8/1pP5/8/8/4P3/4K3 b q - 1 1", -25 },
-    { "r3k2R/6P1/8/1pP5/8/8/4P3/4K3 b q - 0 1", -1277 }
+    { "r3k2b/6P1/1P6/8/8/8/4P3/4K2R b Kq - 0 1", -609 },
+    { "r3k2b/6P1/8/1pP5/8/4P3/8/4K2R b Kq - 0 1", -147 },
+    { "r3k2b/6P1/8/1pP5/4P3/8/8/4K2R b Kq e3 0 1", -125 },
+    { "r3k2b/6P1/2P5/1p6/8/8/4P3/4K2R b Kq - 0 1", -311 },
+    { "r3k2Q/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -2161 },
+    { "r3k2R/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -1705 },
+    { "r3k2B/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -996 },
+    { "r3k2N/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -751 },
+    { "r3k1Qb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -1552 },
+    { "r3k1Rb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -1012 },
+    { "r3k1Bb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", -149 },
+    { "r3k1Nb/8/8/1pP5/8/8/4P3/4K2R b Kq - 0 1", 102 },
+    { "r3k2b/6P1/8/1pP5/8/8/4P3/3K3R b q - 1 1", -120 },
+    { "r3k2b/6P1/8/1pP5/8/8/4P3/5K1R b q - 1 1", -88 },
+    { "r3k2b/6P1/8/1pP5/8/8/3KP3/7R b q - 1 1", -162 },
+    { "r3k2b/6P1/8/1pP5/8/8/4PK2/7R b q - 1 1", -150 },
+    { "r3k2b/6P1/8/1pP5/8/8/4P3/5RK1 b q - 1 1", -112 },
+    { "r3k2b/6P1/8/1pP5/8/8/4P3/4KR2 b q - 1 1", -220 },
+    { "r3k2b/6P1/8/1pP5/8/8/4P3/4K1R1 b q - 1 1", -165 },
+    { "r3k2b/6P1/8/1pP5/8/8/4P2R/4K3 b q - 1 1", -162 },
+    { "r3k2b/6P1/8/1pP5/8/7R/4P3/4K3 b q - 1 1", -212 },
+    { "r3k2b/6P1/8/1pP5/7R/8/4P3/4K3 b q - 1 1", -237 },
+    { "r3k2b/6P1/8/1pP4R/8/8/4P3/4K3 b q - 1 1", -250 },
+    { "r3k2b/6P1/7R/1pP5/8/8/4P3/4K3 b q - 1 1", -297 },
+    { "r3k2b/6PR/8/1pP5/8/8/4P3/4K3 b q - 1 1", -310 },
+    { "r3k2R/6P1/8/1pP5/8/8/4P3/4K3 b q - 0 1", -1336 }
 };
 
 void testAccumulatorUpdate(Board board, Move move) 
