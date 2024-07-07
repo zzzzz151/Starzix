cc8b2325ed3cae6ef85c529f4ed7e0fdbbba8182
zzzzz <ricardopinto15@hotmail.com>
2024-07-07 20:15:42 +0100

    mvvlva (#22)
    bench 126873410

--- src/ply_data.hpp
+++ src/ply_data.hpp
@@ -11,6 +11,8 @@ struct PlyData {
 
     MoveGenType mMovesGenerated = MoveGenType::NONE;
     ArrayVec<Move, 256> mMoves;
+    std::array<i32, 256> mMovesScores;
+    int mCurrentMoveIdx = -1;
 
     ArrayVec<Move, MAX_DEPTH+1> mPvLine = {};
 
@@ -20,6 +22,43 @@ struct PlyData {
             board.pseudolegalMoves(mMoves, noisiesOnly, false);
             mMovesGenerated = (MoveGenType)noisiesOnly;
         }
+
+        // Score moves
+
+        int stm = (int)board.sideToMove();
+
+        for (size_t i = 0; i < mMoves.size(); i++)
+        {
+            Move move = mMoves[i];
+            PieceType captured = board.captured(move);
+
+            mMovesScores[i] = move.promotion() == PieceType::QUEEN ? 1'000'000 : 0;
+
+            if (captured != PieceType::NONE)
+            {
+                mMovesScores[i] += 100 * (i32)captured - (i32)move.pieceType();
+            }
+        }
+
+        mCurrentMoveIdx = -1; // prepare for moves loop
+    }
+
+    inline std::pair<Move, i32> nextMove()
+    {
+        mCurrentMoveIdx++;
+
+        if (mCurrentMoveIdx >= (int)mMoves.size())
+            return { MOVE_NONE, 0 };
+
+        // Incremental sort
+        for (size_t j = mCurrentMoveIdx + 1; j < mMoves.size(); j++)
+            if (mMovesScores[j] > mMovesScores[mCurrentMoveIdx])
+            {
+                mMoves.swap(mCurrentMoveIdx, j);
+                std::swap(mMovesScores[mCurrentMoveIdx], mMovesScores[j]);
+            }
+
+        return { mMoves[mCurrentMoveIdx], mMovesScores[mCurrentMoveIdx] };
     }
 
     inline void updatePV(Move move) 
