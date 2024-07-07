d0ab27294a671f5d3e51de5ebb7f0b3df0bfe6e2
zzzzz <ricardopinto15@hotmail.com>
2024-07-07 19:24:00 +0100

    SEE move ordering
    bench 62379146

--- src/ply_data.hpp
+++ src/ply_data.hpp
@@ -18,6 +18,8 @@ struct PlyData {
 
     inline void genAndScoreMoves(Board &board, bool noisiesOnly) 
     {
+        // Generate moves if not already generated
+        // Never generate underpromotions in search
         if (mMovesGenerated != (MoveGenType)noisiesOnly) {
             board.pseudolegalMoves(mMoves, noisiesOnly, false);
             mMovesGenerated = (MoveGenType)noisiesOnly;
@@ -31,13 +33,26 @@ struct PlyData {
         {
             Move move = mMoves[i];
             PieceType captured = board.captured(move);
+            PieceType promotion = move.promotion();
 
-            mMovesScores[i] = move.promotion() == PieceType::QUEEN ? 1'000'000 : 0;
+            // Starzix doesnt generate underpromotions in search
+            assert(promotion == PieceType::NONE || promotion == PieceType::QUEEN);
+
+            mMovesScores[i] = 0;
 
             if (captured != PieceType::NONE)
             {
+                mMovesScores[i] = SEE(board, move) 
+                                  ? (promotion == PieceType::QUEEN
+                                     ? GOOD_QUEEN_PROMO_SCORE 
+                                     : GOOD_NOISY_SCORE)
+                                  : -GOOD_NOISY_SCORE;
+
+                // MVV (most valuable victim)
                 mMovesScores[i] += 100 * (i32)captured - (i32)move.pieceType();
             }
+            else if (promotion == PieceType::QUEEN)
+                mMovesScores[i] = SEE(board, move) ? GOOD_QUEEN_PROMO_SCORE : -GOOD_NOISY_SCORE;
         }
 
         mCurrentMoveIdx = -1; // prepare for moves loop
