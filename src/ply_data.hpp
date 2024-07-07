f20534ac245428ef8811977e7a0ee3423beb4bdb
zzzzz <ricardopinto15@hotmail.com>
2024-07-07 18:42:18 +0100

    depth 2 (#18)
    bench 1

--- src/ply_data.hpp
+++ src/ply_data.hpp
@@ -2,8 +2,23 @@
 
 #pragma once
 
+enum class MoveGenType : i8 {
+    NONE = -1, ALL = 0, NOISIES_ONLY = 1
+};
+
 struct PlyData {
     public:
 
+    MoveGenType mMovesGenerated = MoveGenType::NONE;
+    ArrayVec<Move, 256> mMoves;
+
     ArrayVec<Move, MAX_DEPTH+1> mPvLine = {};
+
+    inline void genAndScoreMoves(Board &board, bool noisiesOnly) 
+    {
+        if (mMovesGenerated != (MoveGenType)noisiesOnly) {
+            board.pseudolegalMoves(mMoves, noisiesOnly, false);
+            mMovesGenerated = (MoveGenType)noisiesOnly;
+        }
+    }
 };
