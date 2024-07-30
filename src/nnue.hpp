3279e408189c0b0c0ee8bdec82c4697477de03f5
zzzzz <ricardopinto15@hotmail.com>
2024-07-30 12:19:13 +0100

    Original SIMD
    bench 6935430

--- src/nnue.hpp
+++ src/nnue.hpp
@@ -12,29 +12,27 @@
 #include "board.hpp"
 #include "search_params.hpp"
 #include "simd.hpp"
-using namespace SIMD;
 
 namespace nnue {
 
 constexpr i32 HIDDEN_LAYER_SIZE = 1024,
               SCALE = 400, 
               QA = 255, 
-              QB = 64,
-              WEIGHTS_PER_VEC = sizeof(Vec) / sizeof(i16);
+              QB = 64;
 
-struct alignas(ALIGNMENT) Net {
+struct Net {
     public:
-    MultiArray<i16, 2, 768, HIDDEN_LAYER_SIZE> featuresWeights; // [color][feature][hiddenNeuron]
-    MultiArray<i16, 2, HIDDEN_LAYER_SIZE>      hiddenBiases;    // [color][hiddenNeuron]
-    MultiArray<i16, 2, HIDDEN_LAYER_SIZE>      outputWeights;   // [color][hiddenNeuron]
+    MultiArray<i16, 2, 768, HIDDEN_LAYER_SIZE> featuresWeights;   // [color][feature][hiddenNeuron]
+    MultiArray<i16, 2, HIDDEN_LAYER_SIZE>      hiddenBiases;      // [color][hiddenNeuron]
+    std::array<i16, HIDDEN_LAYER_SIZE>         outputWeightsStm;  // [hiddenNeuron]
+    std::array<i16, HIDDEN_LAYER_SIZE>         outputWeightsNstm; // [hiddenNeuron]
     i16                                        outputBias;
 };
 
 INCBIN(NetFile, "src/net.bin");
 const Net* NET = (const Net*)gNetFileData;
 
-struct alignas(ALIGNMENT) Accumulator
-{
+struct Accumulator {
     public:
 
     MultiArray<i16, 2, HIDDEN_LAYER_SIZE> mAccumulators;
@@ -191,33 +189,66 @@ inline i32 evaluate(Accumulator* accumulator, Board &board, bool materialScale)
     assert(accumulator->mUpdated);
 
     int stm = (int)board.sideToMove();
-    Vec* stmAccumulator = (Vec*) &(accumulator->mAccumulators[stm]);
-    Vec* oppAccumulator = (Vec*) &(accumulator->mAccumulators[!stm]);
+    i32 sum = 0;
 
-    Vec* stmWeights = (Vec*) &(NET->outputWeights[0]);
-    Vec* oppWeights = (Vec*) &(NET->outputWeights[1]);
-    const Vec vecZero = vecSetZero();
-    const Vec vecQA = vecSet1Epi16(QA);
-    Vec sum = vecSetZero();
-    Vec v0, v1;
+    // Activation function:
+    // SCReLU(hiddenNeuron) = clamp(hiddenNeuron, 0, QA)^2
 
-    for (int i = 0; i < HIDDEN_LAYER_SIZE / WEIGHTS_PER_VEC; ++i) {
-        // Side to move
-        v0 = maxEpi16(stmAccumulator[i], vecZero); // clip
-        v0 = minEpi16(v0, vecQA); // clip
-        v1 = mulloEpi16(v0, stmWeights[i]); // square
-        v1 = maddEpi16(v1, v0); // multiply with output layer
-        sum = addEpi32(sum, v1); // collect the result
+    #if defined(__AVX2__) || (defined(__AVX512F__) && defined(__AVX512BW__))
+        // N is 32 if avx512 else 16 if avx2
 
-        // Non side to move
-        v0 = maxEpi16(oppAccumulator[i], vecZero);
-        v0 = minEpi16(v0, vecQA);
-        v1 = mulloEpi16(v0, oppWeights[i]);
-        v1 = maddEpi16(v1, v0);
-        sum = addEpi32(sum, v1);
-    }
+        const Vec vecZero = setEpi16(0);  // N i16 zeros
+        const Vec vecQA   = setEpi16(QA); // N i16 QA's
 
-    i32 eval = (vecHaddEpi32(sum) / QA + NET->outputBias) * SCALE / (QA * QB);
+        // Takes N i16 hidden neurons, along with N i16 output weights
+        // Activates the hidden neurons (SCReLU) and multiplies each with its output weight
+        // Finally, adds the resulting N i16's to the "sum" variable
+        auto sumSlice = [&](const i16 &accumulatorStart, const i16 &outputWeightsStart) -> void
+        {
+            // Load N hidden neurons and clamp them to [0, QA]
+            Vec hiddenNeurons = loadVec((Vec*)&accumulatorStart);
+            hiddenNeurons = clampVec(hiddenNeurons, vecZero, vecQA);
+
+            // Load the respective N output weights
+            Vec outputWeights = loadVec((Vec*)&outputWeightsStart);
+
+            // Multiply each hidden neuron with its respective output weight
+            // We use mullo, which multiplies in the i16 world
+            // since we know the results fit in an i16
+            Vec result = mulloEpi16(hiddenNeurons, outputWeights);
+
+            // Multiply with hidden neurons again (square part of SCReLU)
+            // We use madd, which multiplies in the i32 world
+            result = maddEpi16(result, hiddenNeurons);
+
+            // Sum the resulting N i32's, and add that sum to the "sum" variable
+            sum += sumVec(result);
+        };
+
+        for (int i = 0; i < HIDDEN_LAYER_SIZE; i += sizeof(Vec) / sizeof(i16))
+        {
+            // From each accumulator, take the next N i16 hidden neurons 
+            // and their i16 output weights (1 per hidden neuron, so N)
+            sumSlice(accumulator->mAccumulators[stm][i], NET->outputWeightsStm[i]);
+            sumSlice(accumulator->mAccumulators[!stm][i], NET->outputWeightsNstm[i]);
+        }
+    #else
+        for (int i = 0; i < HIDDEN_LAYER_SIZE; i++)
+        {
+            i16 clipped = std::clamp<i16>(accumulator->mAccumulators[stm][i], 0, QA);
+            i16 x = clipped * NET->outputWeightsStm[i];
+            sum += x * clipped;
+        }
+
+        for (int i = 0; i < HIDDEN_LAYER_SIZE; i++)
+        {
+            i16 clipped = std::clamp<i16>(accumulator->mAccumulators[!stm][i], 0, QA);
+            i16 x = clipped * NET->outputWeightsNstm[i];
+            sum += x * clipped;
+        }
+    #endif
+
+    i32 eval = (sum / QA + NET->outputBias) * SCALE / (QA * QB);
 
     // Scale eval with material / game phase
     if (materialScale) {
--- src/simd.hpp
+++ src/simd.hpp
@@ -2,170 +2,94 @@
 
 #pragma once
 
-#include <immintrin.h>
+#if (defined(__AVX512F__) && defined(__AVX512BW__)) || defined(__AVX2__)
+    #include <immintrin.h>
 
-namespace SIMD {
+    #if defined(__AVX512F__) && defined(__AVX512BW__)
+        using Vec = __m512i;
+    #else // AVX2
+        using Vec = __m256i;
+    #endif
 
-#if defined(__AVX512F__) && defined(__AVX512BW__)
+    inline Vec setEpi16(i16 x) 
+    {
+        #if defined(__AVX512F__) && defined(__AVX512BW__)
+            return _mm512_set1_epi16(x);
+        #else // AVX2
+            return _mm256_set1_epi16(x);
+        #endif
+    }
 
-using Vec = __m512i;
+    inline Vec loadVec(Vec* vecPtr)
+    {
+    	#if defined(__AVX512F__) && defined(__AVX512BW__)
+            return _mm512_load_si512(vecPtr);
+        #else // AVX2
+            return _mm256_load_si256(vecPtr);
+        #endif
+    }
 
-inline Vec addEpi16(Vec x, Vec y) {
-    return _mm512_add_epi16(x, y);
-}
+    inline Vec clampVec(Vec vec, Vec minVec, Vec maxVec)
+    {
+        #if defined(__AVX512F__) && defined(__AVX512BW__)
+            return _mm512_min_epi16(_mm512_max_epi16(vec, minVec), maxVec);
+        #else // AVX2
+            return _mm256_min_epi16(_mm256_max_epi16(vec, minVec), maxVec);
+        #endif
+    }
 
-inline Vec addEpi32(Vec x, Vec y) {
-    return _mm512_add_epi32(x, y);
-}
+    inline Vec mulloEpi16(Vec a, Vec b)
+    {
+        #if defined(__AVX512F__) && defined(__AVX512BW__)
+            return _mm512_mullo_epi16(a, b);
+        #else // AVX2
+            return _mm256_mullo_epi16(a, b);
+        #endif
+    }
 
-inline Vec subEpi16(Vec x, Vec y) {
-    return _mm512_sub_epi16(x, y);
-}
+    inline Vec maddEpi16(Vec a, Vec b)
+    {
+        #if defined(__AVX512F__) && defined(__AVX512BW__)
+            return _mm512_madd_epi16(a, b);
+        #else // AVX2
+            return _mm256_madd_epi16(a, b);
+        #endif
+    }
 
-inline Vec minEpi16(Vec x, Vec y) {
-    return _mm512_min_epi16(x, y);
-}
+    // Adds the i16's in vec, returning an i32
+    inline i32 sumVec(Vec vec) 
+    {
+        #if defined(__AVX512F__) && defined(__AVX512BW__)
+            const Vec high64 = _mm_unpackhi_epi64(vec, vec);
+            const Vec sum64 = _mm_add_epi32(vec, high64);
 
-inline Vec maxEpi16(Vec x, Vec y) {
-    return _mm512_max_epi16(x, y);
-}
+            const Vec high32 = _mm_shuffle_epi32(sum64, _MM_SHUFFLE(2, 3, 0, 1));
+            const Vec sum32 = _mm_add_epi32(sum64, high32);
 
-inline Vec mulloEpi16(Vec x, Vec y) {
-    return _mm512_mullo_epi16(x, y);
-}
+            return _mm_cvtsi128_si32(sum32);
+        #else // AVX2
+            // Get the lower and upper half of the register:
+            __m128i xmm0 =  _mm256_castsi256_si128(vec);
+            __m128i xmm1 =  _mm256_extracti128_si256(vec, 1);
 
-inline Vec maddEpi16(Vec x, Vec y) {
-    return _mm512_madd_epi16(x, y);
-}
+            // Add the lower and upper half vertically:
+            xmm0 = _mm_add_epi32(xmm0, xmm1);
 
-inline Vec vecSetZero() {
-    return _mm512_setzero_si512();
-}
+            // Get the upper half of the result:
+            xmm1 = _mm_unpackhi_epi64(xmm0, xmm0);
 
-inline Vec vecSet1Epi16(int16_t x) {
-    return _mm512_set1_epi16(x);
-}
+            // Add the lower and upper half vertically:
+            xmm0 = _mm_add_epi32(xmm0, xmm1);
 
-inline int vecHaddEpi32(Vec vec) {
-    return _mm512_reduce_add_epi32(vec);
-}
+            // Shuffle the result so that the lower 32-bits are directly above the second-lower 32-bits:
+            xmm1 = _mm_shuffle_epi32(xmm0, _MM_SHUFFLE(2, 3, 0, 1));
 
-#elif defined(__AVX2__)
+            // Add the lower 32-bits to the second-lower 32-bits vertically:
+            xmm0 = _mm_add_epi32(xmm0, xmm1);
 
-using Vec = __m256i;
-
-inline Vec addEpi16(Vec x, Vec y) {
-    return _mm256_add_epi16(x, y);
-}
-
-inline Vec addEpi32(Vec x, Vec y) {
-    return _mm256_add_epi32(x, y);
-}
-
-inline Vec subEpi16(Vec x, Vec y) {
-    return _mm256_sub_epi16(x, y);
-}
-
-inline Vec minEpi16(Vec x, Vec y) {
-    return _mm256_min_epi16(x, y);
-}
-
-inline Vec maxEpi16(Vec x, Vec y) {
-    return _mm256_max_epi16(x, y);
-}
-
-inline Vec mulloEpi16(Vec x, Vec y) {
-    return _mm256_mullo_epi16(x, y);
-}
-
-inline Vec maddEpi16(Vec x, Vec y) {
-    return _mm256_madd_epi16(x, y);
-}
-
-inline Vec vecSetZero() {
-    return _mm256_setzero_si256();
-}
-
-inline Vec vecSet1Epi16(int16_t x) {
-    return _mm256_set1_epi16(x);
-}
-
-inline int vecHaddEpi32(Vec vec) {
-    // Get the lower and upper half of the register:
-    __m128i xmm0 =  _mm256_castsi256_si128(vec);
-    __m128i xmm1 =  _mm256_extracti128_si256(vec, 1);
-
-    // Add the lower and upper half vertically:
-    xmm0 = _mm_add_epi32(xmm0, xmm1);
-
-    // Get the upper half of the result:
-    xmm1 = _mm_unpackhi_epi64(xmm0, xmm0);
-
-    // Add the lower and upper half vertically:
-    xmm0 = _mm_add_epi32(xmm0, xmm1);
-
-    // Shuffle the result so that the lower 32-bits are directly above the second-lower 32-bits:
-    xmm1 = _mm_shuffle_epi32(xmm0, _MM_SHUFFLE(2, 3, 0, 1));
-
-    // Add the lower 32-bits to the second-lower 32-bits vertically:
-    xmm0 = _mm_add_epi32(xmm0, xmm1);
-
-    // Cast the result to the 32-bit integer type and return it:
-    return _mm_cvtsi128_si32(xmm0);
-}
-
-#else
-
-using Vec = __m128i;
-
-inline Vec addEpi16(Vec x, Vec y) {
-    return _mm_add_epi16(x, y);
-}
-
-inline Vec addEpi32(Vec x, Vec y) {
-    return _mm_add_epi32(x, y);
-}
-
-inline Vec subEpi16(Vec x, Vec y) {
-    return _mm_sub_epi16(x, y);
-}
-
-inline Vec minEpi16(Vec x, Vec y) {
-    return _mm_min_epi16(x, y);
-}
-
-inline Vec maxEpi16(Vec x, Vec y) {
-    return _mm_max_epi16(x, y);
-}
-
-inline Vec mulloEpi16(Vec x, Vec y) {
-    return _mm_mullo_epi16(x, y);
-}
-
-inline Vec maddEpi16(Vec x, Vec y) {
-    return _mm_madd_epi16(x, y);
-}
-
-inline Vec vecSetZero() {
-    return _mm_setzero_si128();
-}
-
-inline Vec vecSet1Epi16(int16_t x) {
-    return _mm_set1_epi16(x);
-}
-
-inline int vecHaddEpi32(Vec vec) {
-    const Vec high64 = _mm_unpackhi_epi64(vec, vec);
-    const Vec sum64 = _mm_add_epi32(vec, high64);
-
-    const Vec high32 = _mm_shuffle_epi32(sum64, _MM_SHUFFLE(2, 3, 0, 1));
-    const Vec sum32 = _mm_add_epi32(sum64, high32);
-
-    return _mm_cvtsi128_si32(sum32);
-}
+            // Cast the result to the 32-bit integer type and return it:
+            return _mm_cvtsi128_si32(xmm0);
+        #endif
+    }
 
 #endif
-
-constexpr int ALIGNMENT = std::max<int>(8, sizeof(Vec));
-
-} // namespace SIMD
