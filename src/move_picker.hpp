b69ad7aa06c20a25d52339e26fc9e34f82667969
zzzzz151 <ricardopinto15@hotmail.com>
2025-02-05 01:19:03 +0000

    MVVLVA
    bench 214872923

--- src/move_picker.hpp
+++ src/move_picker.hpp
@@ -7,11 +7,28 @@
 #include "position.hpp"
 #include "move_gen.hpp"
 
+constexpr Move partialSelectionSort(
+    ArrayVec<Move, 256>& moves, std::array<i32, 256>& movesScores, const size_t idx)
+{
+    assert(idx < moves.size());
+
+    for (size_t i = idx + 1; i < moves.size(); i++)
+        if (movesScores[i] > movesScores[idx])
+        {
+            moves.swap(i, idx);
+            std::swap(movesScores[i], movesScores[idx]);
+        }
+
+    return moves[idx];
+}
+
 struct MovePicker
 {
 public:
 
     ArrayVec<Move, 256> mNoisies, mQuiets;
+
+    std::array<i32, 256> mNoisiesScores, mQuietsScores;
 
     i32 mNoisiesIdx = -1, mQuietsIdx = -1;
 
@@ -42,11 +59,33 @@ public:
         }());
 
         if (mNoisiesIdx == -1)
+        {
             mNoisies = pseudolegalMoves(pos, MoveGenType::NoisyOnly);
+
+            for (size_t i = 0; i < mNoisies.size(); i++)
+            {
+                const Move move = mNoisies[i];
+                const std::optional<PieceType> promo = move.promotion();
+
+                mNoisiesScores[i] = promo && *promo == PieceType::Queen ? 10'000
+                                  : promo ? -10'000
+                                  : 0;
+
+                // MVVLVA
+
+                const std::optional<PieceType> captured = pos.captured(move);
+                const i32 iCaptured  = (captured ? static_cast<i32>(*captured) : 0) + 1;
+                const i32 iPieceType = static_cast<i32>(move.pieceType()) + 1;
+
+                mNoisiesScores[i] += iCaptured * 100 - iPieceType;
+            }
+        }
 
         while (static_cast<size_t>(++mNoisiesIdx) < mNoisies.size())
         {
-            const Move move = mNoisies[static_cast<size_t>(mNoisiesIdx)];
+            const Move move = partialSelectionSort(
+                mNoisies, mNoisiesScores, static_cast<size_t>(mNoisiesIdx)
+            );
 
             if (isPseudolegalLegal(pos, move))
                 return move;
